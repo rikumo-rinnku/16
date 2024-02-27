c466ec62452f6d777022332f03f80301c470887b
rikumo <rikumo@aliyun.com>
2024-02-27 18:09:20 +0800

    项目初始化

---
+++ a.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+void a(){
+    std::cout << "this a" << std::endl;
+    int i = 0;
+    while(i<10){
+        i++;
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+void a();
+
+int main() {
+    cout << "hallo world" << endl;
+    a();
+    return 0;
+}
