3c0e5d2a065181e70717d5613e93b1d73aedb1cd
rikumo <rikumo@aliyun.com>
2024-02-28 22:34:19 +0800

    新增终端控制

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include "terminal.h"
 
-using namespace std;
-
-void a();
-
+using namespace std::chrono_literals;
 int main() {
-    cout << "hallo world" << endl;
-    a();
+    // std::cout << "\033[5;10H" << "\033[38;5;214m" << "hallo woreld" << "\033[10;1H";
+    // tc::move_to(5,10);
+    // tc::set_fore_color(214);
+    // tc::set_back_color(241);
+    // std::cout<<"hello world";
+    // tc::move_to(10,1);
+    tc::hide_cursor();
+    int i = 1;
+    while(true){
+        tc::clean_screen();
+        tc::move_to(i++,10);
+        tc::set_back_color(15);
+        std::cout<<"  ";
+        tc::reset_color();
+        std::cout<<std::flush;
+        std::this_thread::sleep_for(1s);
+    }
+
+
     return 0;
 }
---
+++ terminal.h
@@ -0,0 +1,10 @@
+namespace tc{
+    //控制台函数
+    void move_to(int row,int col);//移动光标到坐标
+    void set_fore_color(int id);//前景色
+    void set_back_color(int id);//背景色
+    void clean_screen();//清屏
+    void reset_color();//重置颜色
+    void hide_cursor();//隐藏光标
+    void show_cursor();//显示光标
+}
